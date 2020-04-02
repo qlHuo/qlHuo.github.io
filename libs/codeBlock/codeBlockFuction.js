a83800f94dd70c5c074434651eb2fbcdc73c610f
qlHuo <1322346367@qq.com>
2020-04-02 23:13:14 +0800

    Site updated: 2020-04-02 23:13:04

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,3 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+// build time:Thu Apr 02 2020 23:13:00 GMT+0800 (GMT+08:00)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
