8ff227d3006bb4856d195706ac5cb59d0023e65c
qlHuo <1322346367@qq.com>
2020-05-02 11:32:19 +0800

    Site updated: 2020-05-02 11:32:08

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,3 +1,3 @@
-// build time:Sat May 02 2020 11:26:33 GMT+0800 (GMT+08:00)
+// build time:Sat May 02 2020 11:32:07 GMT+0800 (GMT+08:00)
 $(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
 //rebuild by neat 
